8f73e5965dfc78f4b06df88f8db1186a2e6461cd
Lionel Mann <lionelmann@gmail.com>
2018-03-17 15:18:17 -0400

    old site setup

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <main>
+    <article>
+    <div class="scrollblock" >
+	    <h2 id="fade-in">DOCUMENTARIAN</h2>
+    </div>
+
+	<div class="content">
+		<p>
+		Nathalie Bibeau is a director and producer of international award-winning productions. She started her career at CBC and and Radio-Canada's landmark series, <i>Canada: A People’s History/Canada: une histoire populaire</i>, and has since produced documentary projects such as <i>8th Fire</i>, nominated for the Canadian Screen Awards, and <i>Africa on the Move</i>, which won the Gemini for Best Documentary Series in 2011. Her work has taken her everywhere from the sub-Arctic to Angola and the swamps of Louisiana. 
+		</p>
+		<p>
+		Her latest role is as the Series Director for <i>Back in Time for Dinner Canada</i> (eOne 3Bird Media / CBC), a 7-episode documentary series going to air in July 2018. Before that, she was a Director for CBC's <i>Keeping Canada Safe</i> and <i>Keeping Canada Alive</i>, and was a key member of the producing team on the highly-acclaimed feature documentary, <i>Sugar Coated</i> (TVO/ Canal D/ ZDF/Arte), which premiered at Hot Docs 2015, screened at festivals around the world, is currently available on Netflix (US/Canada), was nominated for three Canadian Screen Awards, and won the prestigious Donald Brittain Award for Best Social and Political Documentary. She was also the lead producer on the associated digital media project (TELUS/ TVO/ FMC / University of Toronto) that created a very successful website (130,000 users to date) and mobile application, One Sweet App.
+		</p>
+		<p>
+		She has written for the Globe and Mail, hosted the Peter Gerretsen Awards at Ryerson University, and volunteered for War Child in Ethiopia. She holds a B.A. from McGill University, an M.A. in Russian History from the University of Toronto, and is fluent in English, French and Swedish.
+		</p>
+	</div>
+
+
+<div class="social">
+		<a href="http://sugarcoateddoc.com" target="_blank"><i class="fa fa-play-circle fa-5x"></i></a><br>
+		<a href="mailto:nat.bibeau@gmail.com"><i class="fa fa-envelope-o fa-4x"></i></a>
+	</div>
+	<div class="copyright">
+		<small>Photo credit: Julien Mann </small>
+	</div>
+</article>
+
+</main>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
-</script>
+<style>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/style.css';
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
